caad9de26963b8a6342d93dc3506425575333ace
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2023-10-07 10:21:19 +0600

    add aos animation in banner and about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,24 +2,25 @@ import aboutImg from "../../assets/aboutImg.webp"
 import { FaLocationDot} from 'react-icons/fa6';
 
 import { AiFillPhone,AiOutlineMail} from 'react-icons/ai';
+import { Link } from "react-router-dom";
 const About = () => {
     return (
         <div>
-            <div className="flex mx-4 mt-14 mb-4">
-                <div className="w-2/4">
+            <div className="flex mx-4 mt-14 mb-4"> 
+                <div className="w-2/4" data-aos="fade-right" data-aos-delay="300" data-aos-duration="1000">
                     <img src={aboutImg} alt="" />
                 </div>
-                <div className="w-2/4">
-                    <h1 className="text-3xl font-semibold mb-2">About Us</h1>
+                <div className="w-2/4" data-aos="fade-left" data-aos-delay="700" data-aos-duration="1000">
+                    <h1 className="text-3xl font-semibold mb-2"><span className="text-[#FF444A] text-3xl">A</span>bout <span className="text-[#FF444A] text-3xl">U</span>s</h1>
                     <p className="text-[#a1a0a0] mb-2"> From weddings that dance with romance to birthday parties filled with joy, and engagements that sparkle with love, our mission is to transform your special moments into unforgettable memories, meticulously planning and managing every detail to ensure your events are as unique and exceptional as you envisioned.</p>
                     <div className="space-y-3">
-                      <p className="flex gap-2 items-center"><span>< AiFillPhone></AiFillPhone></span> Phone: 0123186549</p>
-                      <p className="flex gap-2 items-center"><span><AiOutlineMail></AiOutlineMail></span> Email: wavedreamers@gmail.com</p>
-                      <p className="flex gap-2 items-center"><span><FaLocationDot></FaLocationDot></span> Location: Dhanmondi,Bangldesh</p>
+                      <p className="flex gap-2 items-center"><span className="text-[#FF444A]">< AiFillPhone></AiFillPhone></span> Phone: 0123186549</p>
+                      <p className="flex gap-2 items-center"><span className="text-[#FF444A]"><AiOutlineMail></AiOutlineMail></span> Email: wavedreamers@gmail.com</p>
+                      <p className="flex gap-2 items-center"><span className="text-[#FF444A]"><FaLocationDot></FaLocationDot></span> Location: Dhanmondi,Bangldesh</p>
                                           
                     </div>
                     
-                    <button className="btn btn-outline btn-info text-lg capitalize mt-4">Start Booking</button>
+                    <Link to='/contact'><button className="btn btn-outline btn-info text-lg capitalize mt-4">Start Booking</button></Link>
                 </div>
 
             </div>
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,19 +1,24 @@
 import bannerImg from "../../assets/Banner.webp"
 const Banner = () => {
-    return (
-        <div>
-            <div className="hero min-h-[500px]" style={{backgroundImage: `url(${bannerImg})`}}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Your Dream Events, Our Expert Touch</h1>
-      <p className="mb-5">Our mission is to transform your special moments into unforgettable memories, meticulously planning and managing every detail to ensure your events are as unique and exceptional as you envisioned.</p>
-        <button className="btn bg-white text-[#e1595b] capitalize text-lg">About Us</button>
-    </div>
-  </div>
-</div>
+  return (
+    <div>
+      <div className="hero min-h-[500px]" style={{ backgroundImage: `url(${bannerImg})` }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md" >
+            <div data-aos="fade-down" data-aos-delay="200" data-aos-duration="1000">
+            <h1 className="mb-5 text-5xl font-bold" >Your Dream Events, Our Expert Touch</h1>
+            <p className="mb-5">Our mission is to transform your special moments into unforgettable memories, meticulously planning and managing every detail to ensure your events are as unique and exceptional as you envisioned.</p>
+            </div>
+            <div  data-aos="fade-up" data-aos-delay="500" data-aos-duration="1000">
+            <button className="btn bg-white text-[#e1595b] capitalize text-lg">About Us</button>
+            </div>
+            
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
