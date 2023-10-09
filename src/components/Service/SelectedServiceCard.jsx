14fbe81c280d7293f428742df46bf7ad4a2c61e1
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2023-10-09 19:17:55 +0600

    edit service card details

--- src/components/Service/SelectedServiceCard.jsx
+++ src/components/Service/SelectedServiceCard.jsx
@@ -5,13 +5,13 @@ const SelectedServiceCard = ({selectService}) => {
 
     return (
         <div >
-            <div className="card max-w-[380px] flex justify-center items-center bg-base-100 shadow-xl">
+            <div className="card max-w-[380px] mx-auto flex justify-center items-center bg-base-100 shadow-xl mb-4">
                 <figure className="px-10 pt-10">
                     <img src={image} alt="images" className="rounded-xl" />
                 </figure>
                 <div className="card-body items-center text-center ">
                     <h2 className="card-title text-[#FF444A] text-2xl font-semibold">{service_name}</h2>
-                    <p className="text-[#333333] w-[250px] md:w-[600px] mx-auto text-justify">{description}</p>
+                    <p className="text-[#333333] max-w-[380px]  text-justify">{description}</p>
                     <p>Price: <span className="text-[#FF444A] font-semibold">{price}</span></p>
                     <p>Venue: <span className="text-[#FF444A] font-semibold">{venue}</span></p>
                 </div>
